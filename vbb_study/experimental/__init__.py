1e64f2df3d6dd1883807fe23fa018b6c9f185887
SamuelMcKena <samuel.mckenna@outlook.com>
2026-08-12 11:21:57 +0100

    Expose Phase 2I closure and longitudinal helpers

--- vbb_study/experimental/__init__.py
+++ vbb_study/experimental/__init__.py
@@ -9,13 +9,35 @@ from .bench_dataset import (
     sha256_file,
     verify_dataset_hashes,
 )
+from .closure import (
+    ExperimentalClosureEvidence,
+    camera_calibration_from_bundle,
+    compare_record_to_simulation,
+    compare_vector_field_record,
+    write_closure_evidence,
+)
+from .longitudinal import (
+    MeasuredLongitudinalEvidence,
+    build_measured_longitudinal_evidence,
+    select_longitudinal_stack,
+    write_measured_longitudinal_evidence,
+)
 
 __all__ = [
+    "ExperimentalClosureEvidence",
     "ExperimentalDataset",
     "ExperimentalFrameRecord",
+    "MeasuredLongitudinalEvidence",
+    "build_measured_longitudinal_evidence",
+    "camera_calibration_from_bundle",
+    "compare_record_to_simulation",
+    "compare_vector_field_record",
     "dataset_readiness",
     "load_experimental_dataset",
     "load_intensity_frame",
+    "select_longitudinal_stack",
     "sha256_file",
     "verify_dataset_hashes",
+    "write_closure_evidence",
+    "write_measured_longitudinal_evidence",
 ]
