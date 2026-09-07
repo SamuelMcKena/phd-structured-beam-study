7f6450260f6d1711158d7437af3d341dbdbf01d8
SamuelMcKena <samuel.mckenna@outlook.com>
2026-09-07 09:39:48 +0100

    Expose Miao hollow-core diagnostics

--- vbb_study/correction/__init__.py
+++ vbb_study/correction/__init__.py
@@ -8,7 +8,15 @@ from .miao_forward_model import (
     apply_phase_error,
     classify_error,
     phase_on_normalised_pupil,
+    primary_edge_mode_scope,
+    published_synthetic_terms,
     synthesize_miao_focal_field,
+)
+from .miao_diagnostics import (
+    first_bright_ring_radius,
+    hollow_core_metrics,
+    oracle_phase_correction,
+    oracle_relative_field_error,
 )
 from .miao_retrieval import (
     FullApertureRetrieval,
@@ -23,8 +31,10 @@ from .miao_retrieval import (
 __all__ = [
     "MIAO_CORRECTABLE_ERROR_CLASSES", "NON_MIAO_ERROR_CLASSES",
     "MiaoSyntheticAberration", "add_detector_ring_contamination", "apply_phase_error",
-    "classify_error", "phase_on_normalised_pupil", "synthesize_miao_focal_field",
-    "FullApertureRetrieval", "PlaneRetrieval", "assemble_full_aperture",
-    "correction_manifest", "fit_plane_adaptive", "interpolate_to_cartesian",
-    "map_input_phase_to_slm2",
+    "classify_error", "phase_on_normalised_pupil", "primary_edge_mode_scope",
+    "published_synthetic_terms", "synthesize_miao_focal_field",
+    "first_bright_ring_radius", "hollow_core_metrics", "oracle_phase_correction",
+    "oracle_relative_field_error", "FullApertureRetrieval", "PlaneRetrieval",
+    "assemble_full_aperture", "correction_manifest", "fit_plane_adaptive",
+    "interpolate_to_cartesian", "map_input_phase_to_slm2",
 ]
